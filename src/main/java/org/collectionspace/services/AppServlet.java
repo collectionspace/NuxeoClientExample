ce2a5dfdf50ba17838c3676d056a33bf9bb3cda4
remillet <remillet@yahoo.com>
2014-12-29 18:28:37 -0800

    NOJIRA: Setting up to launch embedded Nuxeo.

--- src/main/java/org/collectionspace/services/AppServlet.java
+++ src/main/java/org/collectionspace/services/AppServlet.java
@@ -1,6 +1,10 @@
 package org.collectionspace.services;
 
+import java.io.File;
 import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,16 +12,90 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.nuxeo.osgi.application.FrameworkBootstrap;
+
 @SuppressWarnings("serial")
 public class AppServlet extends HttpServlet {
 
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    String destination = "/index.jsp";
+	private Logger logger = LoggerFactory.getLogger(AppServlet.class);
+	private FrameworkBootstrap fb;
+	private static final String CSPACE_NUXEO_HOME = "";
+	private static final String NXSERVER = "C:/dev/tools/apache-tomcat-7.0.57/nxserver";
 
-    RequestDispatcher rd = getServletContext()
-        .getRequestDispatcher(destination);
-    rd.forward(req, resp);
-  }
+	private File getNuxeoServerDir(String serverRootPath) throws IOException {
+		File result = null;
+		String errMsg = null;
+
+		String path = NXSERVER;
+		if (path != null) {
+			File temp = new File(path);
+			if (temp.exists() == true) {
+				result = temp;
+			} else {
+				errMsg = "The Nuxeo EP configuration directory is missing or inaccessible at: '"
+						+ path + "'.";
+			}
+		}
+
+		if (result == null) {
+			if (errMsg == null) {
+				path = path != null ? path : "<empty>";
+				errMsg = "Unknown error trying to find Nuxeo configuration: '"
+						+ CSPACE_NUXEO_HOME + "' = " + path;
+			}
+			throw new IOException(errMsg);
+		}
+
+		return result;
+	}
+
+	private void startNuxeo() throws Exception {
+		File nuxeoHomeDir = getNuxeoServerDir(null);
+
+		if (logger.isInfoEnabled() == true) {
+			logger.info("Starting Nuxeo EP server from configuration at: "
+					+ nuxeoHomeDir.getCanonicalPath());
+		}
+
+		fb = new FrameworkBootstrap(AppServlet.class.getClassLoader(),
+				nuxeoHomeDir);
+		fb.setHostName("Tomcat");
+		fb.setHostVersion("7.0.57");
+		
+		fb.initialize();
+		fb.start();
+	}
+
+	private void excerciseNuxeo() {
+		// Do something
+	}
+
+	private void stopNuxeo() throws Exception {
+		fb.stop();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		//
+		// Start, excercise, and stop Nuxeo
+		//
+		try {
+			startNuxeo();
+			excerciseNuxeo();
+			stopNuxeo();
+		} catch (Exception x) {
+			x.printStackTrace(System.err);  // REM - Replace with log statement
+		}
+
+		//
+		// Original code
+		//
+		String destination = "/index.jsp";
+
+		RequestDispatcher rd = getServletContext().getRequestDispatcher(
+				destination);
+		rd.forward(req, resp);
+	}
 }
